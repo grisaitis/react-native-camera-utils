5db5a5fc3412789442461d7fbbb8c5a0ae9ecd9c
William C Grisaitis <william@grisait.is>
2019-06-28 14:19:36 -0400

    Add some demo code

---
+++ demo_camera_utils.js
@@ -0,0 +1,44 @@
+
+/* this works */
+// console.log('importing RNCameraUtils from library...');
+// import {RNCameraUtils} from 'react-native-camera-utils';
+// var methods = getMethods(NativeModules.RNCameraUtils);
+// console.log(methods);
+// NativeModules.RNCameraUtils.addEvent("some name", "some location");  // return value of this is undefined
+// console.log(1);
+// console.log(NativeModules.RNCameraUtils.getConstants());
+// console.log(2);
+
+// console.log('farro soup');
+// console.log('console.log(NativeModules.RNCameraUtils);');
+// console.log(NativeModules.RNCameraUtils);
+// console.log('console.log(RNCameraUtils);');
+// console.log(RNCameraUtils);
+// console.log('console.log(NativeModules.RNAveragePixelLib);');
+// console.log(NativeModules.RNAveragePixelLib);
+
+// var RNCameraUtils = NativeModules.RNCameraUtils;
+// console.log(typeof RNCameraUtils, getMethods(RNCameraUtils));
+// RNCameraUtils.addEvent('Birthday Party', '4 Privet Drive, Surrey');
+
+// var cameraUtils = new NativeModules.RNCameraUtils();
+
+// https://stackoverflow.com/a/152573/781938
+// function getMethods(obj) {
+//   var result = [];
+//   for (var id in obj) {
+//     try {
+//       if (typeof(obj[id]) == "function") {
+//         result.push(id + ": " + obj[id].toString());
+//       }
+//     } catch (err) {
+//       result.push(id + ": inaccessible");
+//     }
+//   }
+//   // console.log(result);
+//   // console.log('sentinel');
+//   return result;
+// }
+
+  // ios: 'Buongiorno\n' + methods,
+
